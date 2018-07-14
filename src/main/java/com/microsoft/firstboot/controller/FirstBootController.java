cd3c1287eb459b17399fedf2779ce0070d4a3a90
decker <decker_chen@163.com>
2018-07-14 19:29:34 +0800

    Initial commit

---
+++ src/main/java/com/microsoft/firstboot/controller/FirstBootController.java
@@ -0,0 +1,15 @@
+package com.microsoft.firstboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/firstboot")
+public class FirstBootController {
+    @RequestMapping(value = "/sayHello", method = RequestMethod.GET)
+    public String sayHello () {
+        return "hello, this is my first boot program!! and test GIT";
+    }
+
+}
